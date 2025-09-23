a65ec0559407b44943cdf2b42c3f80b7f595131b
ZainabIftikhar <zainabiftikhar87@gmail.com>
2025-09-23 18:43:48 +0500

    update app - (1) add profile + (2) set bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './img/react.svg'
 import siteLogo from '/logo.png'
 import './App.css'
 import Header from './pages/Header.jsx';
+import Profile from './pages/Profile.jsx';
 
 
 /*function App() {
@@ -37,11 +37,12 @@ export default App*/
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-50">
+    <div className="min-h-screen bg-neutral-100">
       <Header />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
           <div className="lg:col-span-3">
+          <Profile />
           </div>
           <div className="lg:col-span-1">
           </div>
