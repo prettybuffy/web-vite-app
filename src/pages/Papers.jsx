4e4274a0121394403017948c5357fbf4c98859a1
ZainabIftikhar <zainabiftikhar87@gmail.com>
2025-09-24 08:20:56 +0500

    upload papers + show paper links, website, doi

--- src/pages/Papers.jsx
+++ src/pages/Papers.jsx
@@ -2,51 +2,59 @@ import React from 'react';
 import pdfLogo from '/pdf_icon.svg'
 
 const papers = [
-  {
-    title: "Therapy as an NLP Task: Psychologists' Comparison of LLMs and Human Peers in CBT",
-    authors: "Zainab Iftikhar, Sean Ransom, Amy Xiao, Nicole Nugent, Jeff Huang",
-    venue: "(under review) 2025",
-    website: "[website]",
-    link: "#"
-  },
-  {
+ {
     title: "How LLM Counselors Violate Ethical Standards in Mental Health Practice: A Practitoner-Informed Framework",
     authors: "Zainab Iftikhar, Amy Xiao, Sean Ransom, Jeff Huang, Harini Suresh",
     venue: "AAAI/AIES 2025",
     /*award: "(Best Paper Award)",*/
-    link: "/src/files/AIES25_Framework.pdf"
+    pdf: "/papers/EthicalFramework_AAAI25.pdf",
+    doi: ""
   },
-    {
+  {
+    title: "Therapy as an NLP Task: Psychologists' Comparison of LLMs and Human Peers in CBT",
+    authors: "Zainab Iftikhar, Sean Ransom, Amy Xiao, Nicole Nugent, Jeff Huang",
+    venue: "(under review) 2025",
+    website: "https://cheeseburgertherapy.org",
+    pdf: "/papers/LLM_Therapy.pdf",
+    doi: "https://arxiv.org/abs/2409.02244"
+  },
+ {
     title: "Tone Indicators: Designing Accessible CMC Cues for Neurodiverse Users",
     authors: "Amy Xiao✧, Zainab Iftikhar✧, Jeff Huang",
     venue: "CSCW 2025",
-    link: "/src/files/AIES25_Framework.pdf"
+    pdf: "/papers/ToneIndicators_CSCW25.pdf",
+    doi: ""
   },
   {
     title: "Machine and Human Understanding of Empathy in Online Peer Support: A Cognitive Behavioral Approach",
     authors: "Sara Syed✧, Zainab Iftikhar✧, Amy Xiao, Jeff Huang",
     venue: "CHI 2024",
-    website: "[website]",
-    link: "#"
+    website: "https://cheeseburgertherapy.org/",
+    pdf: "/papers/DigitalEmpathy_CHI24.pdf",
+    doi: "https://dl.acm.org/doi/10.1145/3613904.3642034"
+
   },
   {
     title: "“Together but not together”: Evaluating Typing Indicators for Interaction-Rich Communication",
     authors: "Zainab Iftikhar, Yumeng Ma, Jeff Huang",
     venue: "CHI 2023",
-    website: "[website]",
-    link: "#"
+    website: "https://github.com/brownhci/live-typing",
+    pdf: "/papers/LiveTyping_CHI23.pdf",
+    doi: "https://dl.acm.org/doi/10.1145/3544548.3581248"
   },
   {
     title: "Technology, Privacy, and User Opinions of COVID-19 Mobile Apps for Contact Tracing: Systematic Search and Content Analysis",
     authors: "Mahmoud Elkhodr, Omar Mubin, Zainab Iftikhar, Maleeha Masood, Belal Alsinglawi, Suleman Shahid, Fady Alnajjar",
     venue: "JMIR 2021",
-    link: "#"
+    pdf: "papers/ContactTracing_JMIR21.pdf",
+    doi: "https://pubmed.ncbi.nlm.nih.gov/33493125/"
   },
   {
     title: "Designing Parental Monitoring and Control Technology: A Systematic Review",
     authors: "Zainab Iftikhar, Qutaiba Rohan ul Haq, Osama Younus, Taha Sardar, Hammad Arif, Mobin Javed, Suleman Shahid",
     venue: "INTERACT 2021",
-    link: "#"
+    pdf: "papers/ParentalControls_INTERACT21.pdf",
+    doi: "https://link.springer.com/chapter/10.1007/978-3-030-85610-6_39"
   }
 ];
 
@@ -60,16 +68,18 @@ export default function Research() {
       <div className="space-y-4">
         {papers.map((paper, index) => (
           <div key={index} className="flex gap-2">
-            <div className="flex-shrink-0">
+            <a href={paper.pdf} className="flex-shrink-0">
               <div className="w-12 h-16 flex items-center justify-center">
                 <img src={pdfLogo} alt="PDF Logo" />
               </div>
-            </div>   
+            </a>   
             
             <div className="flex-1">
+            <a href={paper.doi}>
               <h3 className="font-medium hover:text-pink-600 cursor-pointer">
                 {paper.title}
               </h3>
+            </a>
               <p className="">{paper.authors}</p>
               <div className="flex items-center gap-2">
                 <span className="">{paper.venue}</span>
@@ -77,8 +87,8 @@ export default function Research() {
                   <span className="text-orange-600 font-medium">{paper.award}</span>
                 )}
                 {paper.website && (
-                  <a href={paper.link} className="text-pink-600 hover:underline">
-                    {paper.website}
+                  <a href={paper.website} className="text-pink-600 hover:underline">
+                    [website]
                   </a>
                 )}
               </div>
